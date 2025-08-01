7806ca7a6d5b61eb4fdcfb55eec650b5bcecd39d
bushraalme@gmail.com <bushraalme@gmail.com>
2025-08-01 09:16:57 +0600

    class 2 done

---
+++ Class2/JS1.js
@@ -0,0 +1,10 @@
+//mdn docs- a platform we can learn more about js- https://developer.mozilla.org/en-US/docs/Web/JavaScript
+let mark=78
+result=(mark>=80)?"A+":"A";
+console.log(result);
+if (mark>90){
+    console.log("Exvellent and you have done a very good job!!")
+}
+else{
+    console.log("No you need some more improvement...")
+}
