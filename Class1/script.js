18ab79f6cf63c010e7d3fd1b969a83abd0028552
bushraalme@gmail.com <bushraalme@gmail.com>
2025-08-01 08:33:43 +0600

    with html and css

---
+++ Class1/script.js
@@ -0,0 +1,7 @@
+console.log("Hello, JavaScript!");
+alert("Welcome to JavaScript Class!");
+
+function greet() {
+    let name = document.getElementById("nameInput").value;
+    alert("Hello, " + name + "!");
+  }
