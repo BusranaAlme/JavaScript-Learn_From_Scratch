3aa624efc0201dbeddfc7a8200a13c62ef5d030f
bushraalme@gmail.com <bushraalme@gmail.com>
2025-08-01 08:39:08 +0600

    a new js file- just js

---
+++ Class1/js2.js
@@ -0,0 +1,28 @@
+let namee="Alme"
+console.log("Name ="+ namee)
+
+marks={
+    bn:100,
+    en:90,
+    math:100
+}
+console.log(marks)
+
+let name = "Bushra";
+let age = 21;
+console.log("Hello, " + name + "! You are " + age + " years old.");
+
+let num1 = 10, num2 = 20;
+console.log("Sum:", num1 + num2);
+
+let isOnline = true;
+
+let fruits = ["Mango", "Apple", "Banana"];
+console.log(fruits);
+
+let student = {
+  name: "Bushra",
+  roll: 101,
+  marks: 90
+};
+console.log(student);
